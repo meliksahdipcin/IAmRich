6c5158a0331132cd941ad588047ab7b8253baace
Melikşah Dipçin <meliksahdipcin@gmail.com>
2020-07-12 11:55:30 +0300

    no message

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Melikşah on 2.02.2020.
+//  Copyright © 2020 Melikşah Dipçin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
